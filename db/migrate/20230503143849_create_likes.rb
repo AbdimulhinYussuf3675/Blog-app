462eeadb81e79575e283c6336dc86ba9e62a9218
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-03 23:17:00 +0300

    Update changes

--- db/migrate/20230503143849_create_likes.rb
+++ db/migrate/20230503143849_create_likes.rb
@@ -3,7 +3,6 @@ class CreateLikes < ActiveRecord::Migration[7.0]
     create_table :likes do |t|
       t.references :post, foreign_key: true
       t.references :author, foreign_key: { to_table: 'users' }
-      t.references :post, foreign_key: true
 
       t.timestamps
     end
