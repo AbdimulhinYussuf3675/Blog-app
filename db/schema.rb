6009d4064db0dc091c75c1ce95cc2a7caa045f86
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-04 14:31:49 +0300

    Update schema and db

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_144046) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
+    t.bigint "author_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -30,8 +38,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_144046) do
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  create_table "shortlister_students", primary_key: "student_id", id: :integer, default: nil, force: :cascade do |t|
+    t.string "student_name", limit: 50
+    t.string "student_email", limit: 100
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,8 +54,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_03_144046) do
     t.text "photo"
     t.text "bio"
     t.integer "posts_counter"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_users_on_author_id"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "users", "users", column: "author_id"
 end
