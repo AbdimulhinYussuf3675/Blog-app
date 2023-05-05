314e3b700bc48d4247314eac8f238dcbd5b36b48
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-05 18:06:04 +0300

    Add like rspec test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
-
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { Like.new }
+  it 'Likes should be not be Valid' do
+    expect(subject).to_not be_valid
+  end
+  it 'Post like counter to increment' do
+    subject.post = Post.new(title: 'First Post', text: 'This is the my first post', likes_counter: 0)
+    subject.send(:likes_counter)
+    expect(subject.post.likes_counter).to be(1)
+  end
 end
