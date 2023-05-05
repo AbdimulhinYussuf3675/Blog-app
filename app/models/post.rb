0005ecf020cb817af88774e9b0e3cbddac836f68
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-05 06:43:54 +0300

    Add User,POst,comment and like model associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'post_id'
+  has_many :comments, foreign_key: 'post_id'
+
+  after_save :update_posts_counter
+
+
 end
