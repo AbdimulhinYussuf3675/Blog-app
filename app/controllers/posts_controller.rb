3f58eedcacc08a4febdbb4db4e73b8bcd3cce103
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-12 19:19:46 +0300

    Add like and comment controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,33 @@
 class PostsController < ApplicationController
   def show
     @post = Post.where(author_id: params[:user_id], id: params[:id]).first
+    @comments = @post.comments
   end
 
   def index
+    @user = User.find(params[:user_id])
     @posts = Post.where(author_id: params[:user_id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    @post.author = current_user
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
   end
 end
