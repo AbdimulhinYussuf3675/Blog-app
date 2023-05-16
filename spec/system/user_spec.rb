4df318112a7b5861390b43ac790a279928ccfa7b
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-16 12:59:39 +0300

    Implemented changes

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -41,7 +41,7 @@ describe 'User show page, only test user', type: :feature do
   end
 
   it 'I can see the user\'s profile picture.' do
-    expect(page).to have_css("img[src*='#{@user1['photo']}}']")
+    expect(page).to have_css('.user-img')
   end
 
   it 'I can see the user\'s username.' do
@@ -57,19 +57,23 @@ describe 'User show page, only test user', type: :feature do
   end
 end
 
-decribe 'User show page', type: :feature do
+describe 'User show page', type: :feature do
   before :each do
     @user1 = User.create(id: 1, name: 'user1', photo: 'photo.png', bio: 'this is my bio', posts_counter: 0)
-    @post1 = @user1.posts.create(id: 1, title: 'post1', text: 'this is my text', comment_counter: 0, likes_counter: 0)
-    @post2 = @user1.posts.create(id: 2, title: 'post2', text: 'this is my text', comment_counter: 0, likes_counter: 0)
-    @post3 = @user1.posts.create(id: 3, title: 'post3', text: 'this is my text', comment_counter: 0, likes_counter: 0)
-    @post4 = @user1.posts.create(id: 4, title: 'post4', text: 'this is my text', comment_counter: 0, likes_counter: 0)
+    @post1 = @user1.posts.create(id: 1, title: 'post1', text: 'this is my text1', comments_counter: 0,
+                                 likes_counter: 0, author: @user1)
+    @post2 = @user1.posts.create(id: 2, title: 'post2', text: 'this is my text2', comments_counter: 0,
+                                 likes_counter: 0, author: @user1)
+    @post3 = @user1.posts.create(id: 3, title: 'post3', text: 'this is my text3', comments_counter: 0,
+                                 likes_counter: 0, author: @user1)
+    @post4 = @user1.posts.create(id: 4, title: 'post4', text: 'this is my text4', comments_counter: 0,
+                                 likes_counter: 0, author: @user1)
     @comment1 = @post1.comments.create(id: 1, text: 'this is my text', author: @user1)
 
     visit user_path(@user1)
   end
   it 'I can see the user\'s first 3 posts.' do
-    expect(page).to have_selector('.post-title', count: 3)
+    expect(page).to have_selector('.user-post-title', count: 3)
   end
 
   it 'I can see a button that lets me view all of a user\'s posts.' do
@@ -77,8 +81,8 @@ decribe 'User show page', type: :feature do
   end
 
   it 'When I click a user\'s post, it redirects me to that post\'s show page.' do
-    click_on @post1['title']
-    expect(page).to have_current_path user_post_path(@user1, @post1)
+    page.first(".text#{@post2.id}").click
+    expect(page).to have_current_path user_post_path(@user1, @post2)
   end
 
   it 'When I click to see all posts, it redirects me to the user\'s post\'s index page.' do
