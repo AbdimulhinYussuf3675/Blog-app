4ab9180360c99a32fa944f0a088503124811757c
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-17 23:10:44 +0300

    Update to implement delete method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,17 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  belongs_to :post
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
 
   after_save :comments_counter
+  after_destroy :comments_counter
 
   private
 
   def comments_counter
-    post.increment!(:comments_counter)
+    if destroyed?
+      post.decrement!(:comments_counter)
+    else
+      post.increment!(:comments_counter)
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,26 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :likes
-  has_many :comments
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :posts_counter
+  after_destroy :posts_counter
 
   def recent_comments
-    comments.last(5)
+    comments.order(created_at: :desc).limit(5)
   end
 
   private
 
   def posts_counter
-    author.increment!(:posts_counter)
+    if destroyed?
+      author.decrement!(:posts_counter)
+    else
+      author.increment!(:posts_counter)
+    end
   end
 end
