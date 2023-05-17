c1fd06e9c5602cae7d8a3e695c369d02d04da74b
Anthony Martin <anthonymartin20@gmail.com>
2023-05-17 04:44:24 -0400

    Remove redirection when not logged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
 
   protected
 
@@ -10,16 +9,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
 
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-  end
-
-  def authenticate_user!
-    if user_signed_in?
-      super
-      return
-    end
-
-    allowed_pages = [new_user_session_path, new_user_password_path, new_user_registration_path]
-
-    redirect_to new_user_session_path unless allowed_pages.include?(request.original_fullpath)
   end
 end
