a008b2587934eb581061764e70ce0c0c5b29ee45
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-17 10:59:21 +0300

    Configure divse and user authentification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :photo, :bio, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :photo, :bio, :email, :password, :current_password)
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to new_user_session_path if request.original_fullpath != new_user_session_path
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :like, foreign_key: 'author_id'
 
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
   validates :email, presence: true, uniqueness: true
 
   def recent_posts
