94c9e94710b5e777dd1834e452593dd5c90fb998
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-17 23:13:37 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "users#index"
-  resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :show]
-    end
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create]
+  end
 
-  resources :posts, only: [:new, :create] do
-      resources :comments, only: [:new, :create]
-      resources :likes, only: [:create]
-    end
+  resources :posts, only: %i[index show new create destroy] do
+    resources :comments, only: %i[new create destroy]
+    resources :likes, only: :create
+  end
+
+  root 'users#index'
 end
